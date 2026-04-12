c647ed408e116478b7f63018559d42895051d570
Milena Baruc <milenabaruc072@gmail.com>
2026-04-12 14:24:10 -0300

    feat: loop principal do Dijkstra

--- main.py
+++ main.py
@@ -64,8 +64,37 @@ def get_min_distance_node(distances, unvisited):
     pass
 
 def dijkstra(graph, start_name, end_name):
-    # Etapa 2: logica principal de achar o caminho
-    pass
+    # logica principal de achar o caminho
+    distancias = {v.id: float('infinity') for v in graph}
+    distancias[start_name] = 0
+    predecessores = {v.id: None for v in graph}
+    
+    nao_visitados = [v.id for v in graph]
+    
+    while nao_visitados:
+        # Chama a função que tem no passo 3
+        no_atual = get_min_distance_node(distancias, nao_visitados)
+        
+        if no_atual is None or distancias[no_atual] == float('infinity'):
+            break # Não tem mais como avançar
+            
+        vertice_atual = graph.vertices[no_atual] # Puxando da estrutura da Marys
+        
+        # Simulação do relaxamento de arestas
+        for vizinho_id, peso in vertice_atual.vizinhos.items():
+            if vizinho_id in nao_visitados:
+                nova_dist = distancias[no_atual] + peso
+                if nova_dist < distancias[vizinho_id]:
+                    distancias[vizinho_id] = nova_dist
+                    predecessores[vizinho_id] = no_atual
+                    
+        nao_visitados.remove(no_atual)
+        
+        # Otimização: Se já chegou no destino, pode parar
+        if no_atual == end_name:
+            break
+            
+    return predecessores
 
 def reconstruct_path(predecessors, start_name, end_name):
     # voltar a partir do ultimo no
